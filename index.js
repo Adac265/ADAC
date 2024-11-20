7d2a20e2d1562e8769be2882baeb791f8d45027f
Adac265 <168690226+Adac265@users.noreply.github.com>
2024-11-21 00:43:02 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+
+const inputDisplay = document.getElementById("input");
+const outputDisplay = document.getElementById("output");
+let currentAnswer = 0; 
+
+function appendToDisplay(input){
+    inputDisplay.value += input;
+}
+
+function clearDisplay(){
+    inputDisplay.value = "";
+    outputDisplay.textContent = "0";
+}
+
+function calculate(){
+    try{
+        if(inputDisplay.value !== ""){
+            outputDisplay.textContent = eval(inputDisplay.value);
+            currentAnswer = eval(inputDisplay.value);
+        }
+        else{
+            outputDisplay.textContent = "0";
+            appendToDisplay (null);
+        }
+    }
+    catch(error){
+        inputDisplay.value = "Syntax error";
+    }
+    
+}
+
+function deleteBtn(){
+    inputDisplay.value = "Currently not working.";
+}
+
+function currentAns(){
+    outputDisplay.textContent = currentAnswer;
+}
+
+function powerBtn(callback){
+    inputDisplay.value = "Currently not working.";
+}
