021884914c461f62b30828d33f6161309bb2407a
Adac265 <168690226+Adac265@users.noreply.github.com>
2024-11-24 03:15:39 +0500

    Add files via upload

---
+++ adavin.js
@@ -0,0 +1,13 @@
+const catalogButton = document.querySelector(".cat-btn");
+
+const    price = document.querySelector("#price");
+
+const inventory = {
+    "Iphone 15 pro max": 3500000,
+    "HP ProBook": 750000,
+    "KIC Fridge": 350000
+};
+
+function selectedItem(){
+    
+}
